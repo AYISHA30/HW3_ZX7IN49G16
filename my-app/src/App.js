6886e078ada665179c889fddc732ae7dc17cf061
AYISHA30 <suleimanaishatu30@gmail.com>
2020-08-17 22:13:14 -1200

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NewAssign from './week/Assign.js';
+
+import './week/css/components.css';
+import './week/css/global.css';
+import './week/css/layout.css';
+import './week/css/sample-wallpaper.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <NewAssign/>
+    </div>
+  );
+}
+
+export default App;
